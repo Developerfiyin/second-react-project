8c5c349b5f47133ed15603396d02dd336edd6484
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-09-20 01:06:58 +0100

    added folder, it contains all images needed for the project

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Route, Router } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <Router>
+      <Route></Route>
+    </Router>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
